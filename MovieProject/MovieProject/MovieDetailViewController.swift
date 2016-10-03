a4d727faa6b27f96b19a13412eb466e3df58e763
Uskinn <s.nevzor@yahoo.com>
2016-10-03 17:40:09 -0400

    detailVC created

---
+++ MovieProject/MovieProject/MovieDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MovieDetailViewController.swift
+//  MovieProject
+//
+//  Created by Sergey Nevzorov on 10/3/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    @IBOutlet weak var moviePoster: UIImageView!
+
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var movieTitle: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ }
